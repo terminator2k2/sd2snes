29c933641e888b16d0035781cb47bb879f537dae
terminator2k2 <48845336+terminator2k2@users.noreply.github.com>
2026-05-19 07:32:08 +0100

    Update smc.c

--- src/smc.c
+++ src/smc.c
@@ -259,10 +259,7 @@ void smc_id(snes_romprops_t* props, uint32_t file_offset) {
     case 0x2a: /* SPC7110 */
       if(header->carttype == 0xf5 || header->carttype == 0xf9) {
         props->has_spc7110 = 1;
-        props->error = MENU_ERR_NOIMPL;
-        props->error_param = (uint8_t*)"SPC7110";
         props->fpga_conf = FPGA_SPC7110;
-        props->mapper_id = 5;
       }
       break;
 
